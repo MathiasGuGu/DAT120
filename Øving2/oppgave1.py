d14c39afe1433c5317233effe09e307e1ebe7130
Mathias Gumpen Gundersen <90856007+MathiasGuGu@users.noreply.github.com>
2021-10-22 13:28:40 +0200

    Delete Øving2 directory

--- Øving2/oppgave1.py
+++
@@ -1,5 +0,0 @@
-høyde = int(input("Hvor høy er du?: "))
-if(høyde >= 120):
-    print("Du kan ta karusellen")
-else:
-    print("du kan ikke ta karusellen")
--- Øving2/oppgave2.py
+++
@@ -1,6 +0,0 @@
-alder = int(input("Hvor gammel er kunden? "))
-
-if(alder < 18 or alder >= 67):
-    print("biletten koster 20kr")
-else:
-    print("biletten koster 40kr")
